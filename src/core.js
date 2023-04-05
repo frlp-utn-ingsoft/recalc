ea32f185a0b3d6243e7f9329cb4ddd8914cefcbf
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2023-04-05 06:33:33 -0300

    fix: corrige typo en nombre de funciones y variables

--- src/core.js
+++ src/core.js
@@ -1,7 +1,7 @@
 function add(a, b) {
 }
 
-function substract(a, b) {
+function subtract(a, b) {
 }
 
 function multiply(a, b) {
@@ -15,7 +15,7 @@ function pow(a, b) {
 
 export default {
     add: add,
-    sub: substract,
+    sub: subtract,
     mul: multiply,
     div: divide,
     pow: pow
