76894dc94e80a4864cc37ace6d952b6db2332246
dolodmg <dolo.dominguez05@gmail.com>
2023-04-05 16:51:28 -0300

    Arreglo de bug que solicita parametros demás en pow - Dolores Dominguez y Franco Kral

--- src/cli.js
+++ src/cli.js
@@ -24,11 +24,15 @@ async function loop() {
     const fn = core[fnName];
 
     const firstNum = await readline.question("Ingrese el primer número: ")
-    const secondNum = await readline.question("Ingrese el segundo número: ")
-
-    const result = fn(Number(firstNum), Number(secondNum));
-
-    console.log(result);
+    if (fnName == "pow") {
+        const result = fn(Number(firstNum));
+        console.log(result);
+    }
+    else {
+        const secondNum = await readline.question("Ingrese el segundo número: ")
+        const result = fn(Number(firstNum), Number(secondNum));
+        console.log(result);
+    }
     loop();
 }
 
