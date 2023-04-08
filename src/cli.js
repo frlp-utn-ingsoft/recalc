debe508fb01c84099623c5cce2ae96fd4fb629f3
MondinoPedro <mondinopedro12@gmail.com>
2023-04-08 11:55:36 -0300

    feat: Validacion de valores ingresados por teclado

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import { createInterface } from "node:readline/promises";
 import core from "./core.js"
 
+
 const readline = createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -17,18 +18,35 @@ async function loop() {
             return readline.close();
         }
 
-        const fn = core[fnName]; 
+        const fn = core[fnName];
+        const regex = /[a-z]/
         if(fnName !=='pow'){
-            const firstNum = await readline.question("Ingrese el primer número: ");
-            const secondNum =  await readline.question("Ingrese el segundo número: ")
-         
-            const result =  fn(Number(firstNum), Number(secondNum))
 
-            console.log(result);
+            const firstNum = await readline.question("Ingrese el primer número: ");
+            if(firstNum.match(regex) !== null){
+                console.log('ERROR: Debe ingresar un NUMERO! \n')
+                loop()
+            }else{
+                const secondNum =  await readline.question("Ingrese el segundo número: ")
+                if(secondNum.match(regex) !== null){
+                    console.log('ERROR: Debe ingresar un NUMERO! \n')
+                    loop()
+                }else{
+                    const result =  fn(Number(firstNum), Number(secondNum))
+                    console.log(result);
+                }
+            }
+
         }else{
             const num = await readline.question("Ingrese el número: ");
-            const resultpow = fn(Number(num))
-            console.log(resultpow);
+            if(num.match(regex) !== null){
+                console.log('ERROR: Debe ingresar un NUMERO! \n')
+                loop()
+            }else{
+                const resultpow = fn(Number(num))
+                console.log(resultpow);
+            }
+
         }
 
     }else{
