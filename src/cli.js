0b6554a4c04260871f3733ed0c22b3b2f94a637c
Pablo Salani <salanipablo@gmail.com>
2023-04-10 00:35:55 -0300

    Arreglo Bug que permite ingresar cualquier cosa en funcionalidad

--- src/cli.js
+++ src/cli.js
@@ -1,30 +1,36 @@
-import { createInterface } from "node:readline/promises";
-import core from "./core.js"
+import {createInterface} from "node:readline/promises";
+import core from "./core.js";
 
 const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
-const AVAILABLE_FNS = [...Object.keys(core), 'exit'].join(', ')
+const AVAILABLE_FNS = [...Object.keys(core), "exit"].join(", ");
 
 async function loop() {
-    const fnName = await readline.question(`Ingrese función (${AVAILABLE_FNS}): `)
+  const fnName = await readline.question(
+    `Ingrese función (${AVAILABLE_FNS}): `
+  );
 
-    if (fnName === "exit") {
-        console.log("👋👋👋");
-        return readline.close();
-    }
+  if (fnName === "exit") {
+    console.log("👋👋👋");
+    return readline.close();
+  }
 
+  if (AVAILABLE_FNS.includes(fnName)) {
     const fn = core[fnName];
 
-    const firstNum = await readline.question("Ingrese el primer número: ")
-    const secondNum = await readline.question("Ingrese el primer número: ")
+    const firstNum = await readline.question("Ingrese el primer número: ");
+    const secondNum = await readline.question("Ingrese el primer número: ");
 
     const result = fn(Number(firstNum), Number(secondNum));
 
     console.log(result);
-    loop();
+  } else {
+    console.log(`${fnName} No es una funcion valida`);
+  }
+  loop();
 }
 
 loop();
