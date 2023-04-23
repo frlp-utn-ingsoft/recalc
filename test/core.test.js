cccca7e85ee16d9c26b30b884257e0a199b157ac
santilibano7 <santilibano27@gmail.com>
2023-04-22 22:43:53 -0300

    Fix syntax (subs y add)

--- test/core.test.js
+++ test/core.test.js
@@ -1,21 +1,23 @@
 import core from '../src/core.js'
 
+//Test de Substract
 describe('Subtract', () => {
     test('Deberia 2 - 2 = 0', () => {
         expect(core.sub(2, 2)).toBe(0); 
-    })
+    });
 
     test('Deberia 6 - 4 = 2', () => {
         expect(core.sub(6, 4)).toBe(2); 
-    })
-})
+    });
+});
 
 //Test de Add
-describe('Add' () => {
-	test('Deberia 2 + 2 = 4' () => {
+describe('Add', () => {
+	test('Deberia 2 + 2 = 4', () => {
 		expect(core.add(2, 2)).toBe(4);
-	})
-	test('Deberia 6 + 4 = 10' () => {
+	});
+
+	test('Deberia 6 + 4 = 10', () => {
 		expect(core.add(6, 4)).toBe(10);
-	}]
-})
+	});
+});
