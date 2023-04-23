7adca76cf5397fdffcfbe5e9b899c9c70ff4354a
santilibano7 <santilibano27@gmail.com>
2023-04-22 21:50:51 -0300

    Agrega test para Add

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,13 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+//Test de Add
+describe('Add' () => {
+	test('Deberia 2 + 2 = 4' () => {
+		expect(core.add(2, 2)).toBe(4);
+	})
+	test('Deberia 6 + 4 = 10' () => {
+		expect(core.add(6, 4)).toBe(10);
+	}]
+})
