ff6e2aded51ba11e1e6673331194c46f434acf70
Bautista Dev <76666780+Bautistadev@users.noreply.github.com>
2023-04-25 12:24:56 -0300

    TEST: multiply function

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,22 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Multiply',()=>{
+    test('Deberia 3 * 3 = 9', ()=>{
+        const a = 3;
+        const b = 3;
+
+        const result = core.mul(a,b);
+
+        expect(result).toBe(9);
+    })
+    test('No deberia 3 * 2 = 12',()=>{
+        const a = 3;
+        const b = 2;
+
+        const result = core.mul(a,b);
+
+        expect(result).not.toBe(12);
+    })
+})
