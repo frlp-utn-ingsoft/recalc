d12a020cb20f51f25f44f9220e219dbb333b119a
Ulises Mijael Ben <ulisesmijaelben02@gmail.com>
2023-04-25 11:55:09 -0300

    Agrega test para la funcion div

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,14 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('divide', () => {
+    test('Deberia 2 / 2 = 1', () => {
+        expect(core.div(2, 2)).toBe(1); 
+    })
+
+    test('Deberia 12 / 0 = Math Error', () => {
+        expect(core.div(12, 0)).toBe("Math Error"); 
+    })
+})
+
