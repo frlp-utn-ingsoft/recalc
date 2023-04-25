cfc6a511ce8e4b5c0cbb2eff65fa133b3162ce6e
JuaniiEtche <juanietcheverry8@gmail.com>
2023-04-25 18:45:59 -0300

    Agrego endpoint de multiply

--- src/controllers.js
+++ src/controllers.js
@@ -3,7 +3,7 @@ import core from './core.js';
 
 const router = express.Router();
 
-router.get("/sub/:a/:b", async function (req, res) {
+router.get("/mul/:a/:b", async function (req, res) {
     const params = req.params;
     const a = Number(params.a);
     const b = Number(params.b);
@@ -11,9 +11,23 @@ router.get("/sub/:a/:b", async function (req, res) {
     if (isNaN(a) || isNaN(b)) {
         res.status(400).send('Uno de los parámetros no es un número');
     } else {
-        const result = core.sub(a, b);
+        const result = core.mul(a, b);
         return res.send({ result });
     }
 });
+
+router.get("/mul/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.mul(a, b);
+        return res.send({ result });
+    }
+});
+
 
 export default router;
