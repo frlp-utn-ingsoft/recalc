c5047b57df152f40c728838d5b67cb306d304929
MatiBatista <matiasomarbatista1@gmail.com>
2023-04-25 18:54:26 -0300

    Agrego endpoint de pow

--- src/controllers.js
+++ src/controllers.js
@@ -29,5 +29,16 @@ router.get("/mul/:a/:b", async function (req, res) {
     }
 });
 
+router.get("/pow/:a", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    if (isNaN(a)) {
+        res.status(400).send('La base ingresada por parametro no es un numero');
+    } else {
+        const result = core.pow(a);
+        return res.send({ result });
+    }
+});
+
 
 export default router;
