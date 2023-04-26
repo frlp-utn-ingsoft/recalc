1babc01c26560be2d78a7d32adc2cd1e99d2320c
dolodmg <dolo.dominguez05@gmail.com>
2023-04-26 15:57:52 -0300

    feature: Agrego endpoint para función Div

--- src/controllers.js
+++ src/controllers.js
@@ -43,5 +43,20 @@ router.get("/mul/:a/:b", async function (req, res) {
     }
 });
 
+router.get("/div/:a/:b", async function(req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else if (b === 0) {
+        res.status(400).send('El parámetro b no puede ser cero');
+    } else {
+        const result = core.div(a,b);
+        return res.send({result});
+    }
+});
+
 export default router;
 
