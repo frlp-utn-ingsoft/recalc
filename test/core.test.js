fa5d234f4aeb6a7a0975bf7e049676696279cd44
Jennifer Santillan <jenny_s87_5@hotmail.com>
2023-04-26 20:35:47 -0300

    añade test de multiplicacion y funcion de multiplicacion

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,17 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Multiply', () => {
+    test('Deberia 2 * 2 = 4', () => {
+        expect(core.mul(2, 2)).toBe(4); 
+    })
+
+    test('Deberia 3 * 0 = 0', () => {
+        expect(core.mul(3, 0)).toBe(0); 
+    })
+
+    test('Deberia 2 * -5 = -10', () => {
+        expect(core.mul(2, -5)).toBe(-10); 
+    })
+})
