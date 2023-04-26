7ec97282899389e58088dee0f83c659f13502a7c
santilibano7 <santilibano27@gmail.com>
2023-04-25 23:23:42 -0300

    Feature: Agrego endpoint para Add

--- src/controllers.js
+++ src/controllers.js
@@ -16,4 +16,17 @@ router.get("/sub/:a/:b", async function (req, res) {
     }
 });
 
+router.get("/add/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.add(a, b);
+        return res.send({ result });
+    }
+});
+
 export default router;
