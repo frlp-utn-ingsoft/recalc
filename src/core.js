b8a84d59c316329b6ce098deb1cca3bc98067b92
Tomas Martinez <tomasurielmp@gmail.com>
2023-04-26 21:31:10 -0300

    Formated core.js

--- src/core.js
+++ src/core.js
@@ -6,12 +6,16 @@ function subtract(a, b) {
     return a - b;
 }
 
-function multiply (a, b) {return a*b};
+function multiply(a, b) {
+    return a * b
+}
 
-const divide = (a, b) => a/b;
+function divide(a, b) {
+    return a / b
+}
 
 function pow(a) {
-    return a*a;
+    return a * a;
 }
 
 export default {
