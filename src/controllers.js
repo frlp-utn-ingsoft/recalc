3bceb6a47bc20f6315a8d187929c44e66592705b
Tomas Martinez <tomasurielmp@gmail.com>
2023-04-28 17:54:00 -0300

    Added the div endpoint and a standarization to check for api parameters

--- src/controllers.js
+++ src/controllers.js
@@ -4,16 +4,28 @@ import core from './core.js';
 const router = express.Router();
 
 router.get("/sub/:a/:b", async function (req, res) {
-    const params = req.params;
-    const a = Number(params.a);
-    const b = Number(params.b);
+    const params = getRequestParameters(req, res);
+    const result = core.sub(params.a, params.b);
+    return res.send({result});
+});
 
-    if (isNaN(a) || isNaN(b)) {
-        res.status(400).send('Uno de los parámetros no es un número');
-    } else {
-        const result = core.sub(a, b);
-        return res.send({ result });
+router.get("/div/:a/:b", async function (req, res) {
+    try {
+        const params = getRequestParameters(req, res);
+        const result = core.div(params.a, params.b);
+        return res.send({result})
+    } catch (e) {
+        res.status(400).send(e.message);
     }
 });
+
+
+function getRequestParameters(req) {
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
+    if (isNaN(a) || isNaN(b))
+        throw new Error('Uno o ambos parametros no son un numero.');
+    return {a, b}
+}
 
 export default router;
