f98b39253c894c63263a0650443e6333763196d1
Matias Russo <37017789+MatyRusso22@users.noreply.github.com>
2023-05-03 16:18:19 -0300

    Agregue test de Add

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,23 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Prueba de la función suma', () => {
+
+  // Suma de numeros naturales
+  test('La suma de 2 + 3 debe ser igual a 5', () => {
+    expect(core.add(2, 3)).toBe(5);
+  });
+
+  // Suma con numeros negativos
+  test('La suma de -1 + 1 debe ser igual a 0', () => {
+    expect(core.add(-1, 1)).toBe(0);
+  });
+
+  // Suma de numeros con coma
+  test('La suma de 0.1 + 0.2 debe ser igual a 0.3', () => {
+    expect(core.add(0.1, 0.2)).toBeCloseTo(0.3);
+  });
+
+});
+
