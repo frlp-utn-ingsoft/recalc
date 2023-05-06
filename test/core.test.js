4448a8516d4fbfbce536932e4c208db52abfb655
Torres Martín <martin.torres98@hotmail.com>
2023-05-06 16:02:07 -0300

    Agregué los test para la función de división

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,13 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Division', () => {
+    test('3/4 debería = 0.75', () => {
+        expect(core.div(3, 4)).toBe(0.75);
+    })
+
+    test('(-1)/10 debería = -0.1', () => {
+        expect(core.div(-1,10)).toBe(-0.1);
+    })
+})
