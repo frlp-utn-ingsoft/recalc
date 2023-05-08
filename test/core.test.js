1130d05b051b69eed267b6366aef3e285a7f24a0
Pablo Salani <salanipablo@gmail.com>
2023-05-07 21:43:27 -0300

    Feature-Producto

--- test/core.test.js
+++ test/core.test.js
@@ -1,43 +1,38 @@
-import core from '../src/core.js'
+import core from "../src/core.js";
 
-describe('Subtract', () => {
-    test('Deberia 2 - 2 = 0', () => {
-        expect(core.sub(2, 2)).toBe(0); 
-    })
+describe("Subtract", () => {
+  test("Deberia 2 - 2 = 0", () => {
+    expect(core.sub(2, 2)).toBe(0);
+  });
 
-    test('Deberia 6 - 4 = 2', () => {
-        expect(core.sub(6, 4)).toBe(2); 
-    })
-})
+  test("Deberia 6 - 4 = 2", () => {
+    expect(core.sub(6, 4)).toBe(2);
+  });
+});
 
-
-describe('Prueba de la función suma', () => {
-
+describe("Prueba de la función suma", () => {
   // Suma de numeros naturales
-  test('La suma de 2 + 3 debe ser igual a 5', () => {
+  test("La suma de 2 + 3 debe ser igual a 5", () => {
     expect(core.add(2, 3)).toBe(5);
   });
 
   // Suma con numeros negativos
-  test('La suma de -1 + 1 debe ser igual a 0', () => {
+  test("La suma de -1 + 1 debe ser igual a 0", () => {
     expect(core.add(-1, 1)).toBe(0);
   });
 
   // Suma de numeros con coma
-  test('La suma de 0.1 + 0.2 debe ser igual a 0.3', () => {
+  test("La suma de 0.1 + 0.2 debe ser igual a 0.3", () => {
     expect(core.add(0.1, 0.2)).toBeCloseTo(0.3);
   });
-
 });
 
-
-describe('Prueba de la funcion division', () => {
-
+describe("Prueba de la funcion division", () => {
   //Division de numeros naturales
   test("La division de 4 / 2 debe ser igual a 2", () => {
     expect(core.div(4, 2)).toBe(2);
   });
-  
+
   //Division de un numero negativo y un numero natural
   test("La division de -4 / 2 debe ser igual a -2", () => {
     expect(core.div(-4, 2)).toBe(-2);
@@ -47,10 +42,26 @@ describe('Prueba de la funcion division', () => {
   test("La division de -4 / -2 debe ser igual a 2", () => {
     expect(core.div(-4, -2)).toBe(2);
   });
-  
+
   //Division por cero
   test("La division de un numero por cero, debe arrojar error", () => {
     expect(() => core.div(4, 0)).toThrow("No se puede dividir por 0");
   });
+});
 
-})
+describe("Prueba de la funcion producto", () => {
+  //producto de numeros enteros positivos
+  test("El producto de 4 * 2 debe ser igual a 8", () => {
+    expect(core.mul(4, 2)).toBe(8);
+  });
+
+  //Producto de numeros positivos con numero negativos
+  test("El producto de -4 * 2 debe ser igual a -8", () => {
+    expect(core.mul(-4, 2)).toBe(-8);
+  });
+
+  //producto de dos numeros negativos
+  test("El producto de -10 * -5 debe ser igual a 50", () => {
+    expect(core.mul(-10, -5)).toBe(50);
+  });
+});
