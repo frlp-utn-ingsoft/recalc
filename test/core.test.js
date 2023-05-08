ea4eb045628d616b56d991ef68e4a4007e91b534
LucasMarczuk <118766584+LucasMarczuk@users.noreply.github.com>
2023-05-07 22:08:49 -0300

    Agregue el testing correspondiente a core.test.js

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,22 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Division', () => {
+
+    test('Deberia 2 / 0 = Error', () => {
+        expect(() => div(2, 0).toThrow('No se puede dividir por cero'));
+    })
+
+    test('Deberia 10 / 2 = 5', () => {
+        expect(core.div(10, 2)).toBe(5);
+    })
+
+    test('Deberia 6 / 3 = 2', () => {
+        expect(core.div(6, 3)).toBe(2);
+    })
+
+    test('Deberia 20 / 2 = 10', () => {
+        expect(core.div(20, 2)).toBe(10);
+    })
+})
