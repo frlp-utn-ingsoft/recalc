0274ccf7ea4223e1aef90c06b178d93ab4adbf0e
cintiavalero <cintiavalero2001@gmail.com>
2023-05-16 09:47:58 -0300

    Se corrigió 1 test de la funcion add: test para verificar si los parametros son numeros

--- test/core.test.js
+++ test/core.test.js
@@ -91,30 +91,30 @@ describe('Pow', () => {
   });
 })
 
-describe('Sum', () => {
+describe('Add', () => {
   test('Suma de dos números positivos: 2 + 2 tiene que ser 4', () => {
-    expect(core.sum(2, 2)).toBe(4);
+    expect(core.add(2, 2)).toBe(4);
   });
 
   test('Sumar de dos números negativos: -2 + -2 debería ser -4', () => {
-    expect(core.sum(-2, -2)).toBe(-4);
+    expect(core.add(-2, -2)).toBe(-4);
   });
 
   test('No se permiten valores no numéricos', () => {
     expect(() => {
-      const result = core.sum("asd", 2);
+      const result = core.add("numero", 2);
     }).toThrow();
   });
 
   test('Propiedad conmutativa: 2 + 3 = 3 + 2', () => {
-    expect(core.sum(2, 3)).toBe(core.sum(3, 2));
+    expect(core.add(2, 3)).toBe(core.add(3, 2));
   });
 
   test('Propiedad inversa: 2 + -2 = 0 ', () => {
-    expect(core.sum(2, -2)).toBe(0);
+    expect(core.add(2, -2)).toBe(0);
   });
 
   test('Propiedad de la identidad: 2 + 0 = 2', () => {
-    expect(core.sum(2, 0)).toBe(2);
+    expect(core.add(2, 0)).toBe(2);
   });
 });
