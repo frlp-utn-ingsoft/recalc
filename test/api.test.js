dab0c5514e4ac1c0f1d3391d7a7b90841a8fe19f
Pablo Salani <salanipablo@gmail.com>
2023-05-24 08:42:18 -0300

    agrega test para la funcion add

--- test/api.test.js
+++ test/api.test.js
@@ -10,14 +10,37 @@ describe("API substract", () => {
   test("Deberia responder con un 200 ok", async () => {
     const app = await api.build();
 
-    request(app)
+    const res = await request(app)
       .get("/api/v1/sub/2/1")
       .expect(200)
-      .expect("Content-Type", "application/json; charset=utf-8")
-      .end((err, res) => {
-        if (err) throw err;
+      .expect("Content-Type", "application/json; charset=utf-8");
 
-        expect(res.body.resultado).toEqual(1);
-      });
+    expect(res.body.resultado).toEqual(1);
+  });
+});
+
+describe("API add", () => {
+  test("Deberia responder con un 200 ok", async () => {
+    const app = await api.build();
+
+    const res = await request(app)
+      .get("/api/v1/add/2/1")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toEqual(3);
+  });
+
+  test("Si el segundo parametro es negativo, resultado deberia ser menor al primer parametros", async () => {
+    const app = await api.build();
+    const a = 11;
+    const b = -8;
+
+    const res = await request(app)
+      .get(`/api/v1/add/${a}/${b}`)
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toBeLessThan(a);
   });
 });
