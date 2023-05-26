f6cb90b8b63b6ab1559c5b61d0cdda6890a878e2
Matias Perez <ma7iasperez@gmail.com>
2023-05-26 16:59:20 -0300

    feat: Agregado de un nuevo atributo error al modelo History y su test correspondiente.

--- test/models.test.js
+++ test/models.test.js
@@ -10,11 +10,13 @@ beforeEach(async () => {
 })
 
 describe("History", () => {
+    
     test("Deberia poder crear una resta en el history", async () => {
         await createHistoryEntry({
             firstArg: 2,
             secondArg: 2,
             result: 0,
+            error: null,
             operationName: "SUB"
         })
 
@@ -25,6 +27,30 @@ describe("History", () => {
         expect(histories.length).toEqual(1)
         expect(histories[0].firstArg).toEqual(2)
         expect(histories[0].result).toEqual(0)
+        expect(histories[0].error).toBeNull()
         expect(histories[0].Operation.name).toEqual("SUB")
+        
+    })
+
+    test("Debería poder guardar información de error en el history", async () => {
+        const entry = {
+            firstArg: 5,
+            secondArg: 0, 
+            result: null,
+            operationName: "DIV",
+            error: "División por cero" 
+        };
+    
+        await createHistoryEntry(entry);
+    
+        const histories = await History.findAll({
+        include: [Operation]
+        });
+    
+        expect(histories.length).toEqual(1);
+        expect(histories[0].firstArg).toEqual(5);
+        expect(histories[0].result).toBeNull();
+        expect(histories[0].Operation.name).toEqual("DIV");
+        expect(histories[0].error).toEqual("División por cero"); 
     })
 })
