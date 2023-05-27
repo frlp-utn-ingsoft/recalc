1afc980874ef1d33b2a744311045afcec92ff05f
JuaniiEtche <juanietcheverry8@gmail.com>
2023-05-27 17:48:00 -0300

    Se agregó el test correspondiente para verificar la funcion getAllHistory

--- test/models.test.js
+++ test/models.test.js
@@ -2,7 +2,8 @@ const { seed } = require('../src/seed.js')
 const {
     createHistoryEntry,
     History,
-    Operation
+    Operation,
+    getAllHistory
 } = require('../src/models.js')
 
 beforeEach(async () => {
@@ -26,5 +27,24 @@ describe("History", () => {
         expect(histories[0].firstArg).toEqual(2)
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
+    })
+})
+
+describe("getHistory", () => {
+    test("Deberia traerme todo el historial", async () => {
+
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+
+        const histories2 = await getAllHistory(); 
+
+        expect(histories.length).toEqual(histories2.length)
+
+        for (let i = 0; i < histories.length; i++) {
+            expect(histories[i].firstArg).toEqual(histories2[i].firstArg)
+            expect(histories[i].result).toEqual(histories2[i].result)
+            expect(histories[i].Operation.name).toEqual(histories2[i].Operation.name)
+        }
     })
 })
