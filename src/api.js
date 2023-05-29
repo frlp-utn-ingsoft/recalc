320e6192bbfb8fe549172a96f3fd9de0649ff547
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2023-05-29 17:47:58 -0300

    Agrega docker

--- src/api.js
+++ src/api.js
@@ -2,13 +2,15 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import controllers from './controllers.js';
 
+const id = Math.random()
+
 export async function build() {
     const app = express();
 
     app.use(bodyParser.json())
 
     app.get('/', (req, res) => {
-        res.send({ ok: true })
+        res.send({ id })
     })
 
     app.use("/api/v1", controllers);
