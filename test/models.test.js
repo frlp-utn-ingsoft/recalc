5e0a611667f9ac3016fd85041513bb19da798c67
Torres Martín <martin.torres98@hotmail.com>
2023-05-29 21:59:52 -0300

    Agregué el test para comprobar que se obtiene el historial de la base de datos

--- test/models.test.js
+++ test/models.test.js
@@ -2,8 +2,10 @@ const { seed } = require('../src/seed.js')
 const {
     createHistoryEntry,
     History,
-    Operation
+    Operation,
+    getHistory
 } = require('../src/models.js')
+const { json } = require('body-parser')
 
 beforeEach(async () => {
     await seed()
@@ -26,5 +28,57 @@ describe("History", () => {
         expect(histories[0].firstArg).toEqual(2)
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
+    })
+
+    test("Debería traer el historial desde la DB", async () => {
+        //Primero creo entradas en la base de datos
+
+        const entries = [
+            {
+              firstArg: 45,
+              secondArg: 14,
+              operationName: 'ADD',
+              result: 59
+            },
+            {
+              firstArg: 9,
+              secondArg: 3,
+              operationName: 'DIV',
+              result: 3
+            },
+            {
+              firstArg: 7,
+              secondArg: 2,
+              operationName: 'MUL',
+              result: 14
+            }
+          ];
+      
+          // Crear las entradas de prueba en la base de datos
+          for(let i = 0; i < entries.length; i++)
+          {
+            await createHistoryEntry(entries[i])
+          }
+
+        //Obtengo el historial
+        const dbHistory = await getHistory()
+
+        //compruebo que el historial me devuelva un objeto
+        expect(dbHistory).toEqual(expect.any(Object))
+
+        //compruebo que devuelva al menos un objeto        
+        expect(dbHistory.length).toBeGreaterThan(0)
+
+        //compruebo que el objeto esté definido
+        for (let i = 0; i < dbHistory.length; i++) {
+            const element = dbHistory[i];
+
+            expect(element.firstArg).toBeDefined()
+            expect(element.secondArg).toBeDefined()
+            expect(element.result).toBeDefined()
+            expect(element.operationName).toBeDefined()
+            
+        }
+        
     })
 })
