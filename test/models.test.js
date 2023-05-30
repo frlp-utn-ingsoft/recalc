3c1933025aba7113b577813e345bc5d1f42e664d
mmilagros.96m <mmilagros.96m@gmail.com>
2023-05-30 16:45:20 -0300

    test que verifica que el segundo parametro se guarde en la bd

--- test/models.test.js
+++ test/models.test.js
@@ -24,8 +24,25 @@ describe("History", () => {
 
         expect(histories.length).toEqual(1)
         expect(histories[0].firstArg).toEqual(2)
-        expect(histories[0].secondArg).toEqual(2)
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
     })
+
+    test("Deberia poder guardar el segundo parametro en la base de datos", async () => {
+        await createHistoryEntry({
+            firstArg: 10,
+            secondArg: 8,
+            result: 2,
+            operationName: "SUB"
+        })
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+        expect(histories.length).toEqual(1)
+        expect(histories[0].firstArg).toEqual(10)
+        expect(histories[0].secondArg).toEqual(8)
+        expect(histories[0].result).toEqual(2)
+        expect(histories[0].Operation.name).toEqual("SUB")
+    })
 })
+
