e9d45c30ebfad4078feed87ac1f53910297a7aea
fidelaguirre <fidelaguirre1998@gmail.com>
2023-05-30 21:12:18 -0300

    Agregué el test para verificar que el error se guarda correctamente

--- test/models.test.js
+++ test/models.test.js
@@ -26,5 +26,24 @@ describe("History", () => {
         expect(histories[0].firstArg).toEqual(2)
         expect(histories[0].result).toEqual(0)
         expect(histories[0].Operation.name).toEqual("SUB")
+    });
+
+    test("Deberia poder guardar un error en el history", async () => {
+        await createHistoryEntry({
+            firstArg: 2,
+            secondArg: "a",
+            error: "Mensaje de Error",
+            operationName: "DIV"
+        })
+
+        const histories = await History.findAll({
+            include: [Operation]
+        })
+
+        expect(histories.length).toEqual(1)
+        expect(histories[0].error).toEqual("Mensaje de Error")
+        expect(histories[0].firstArg).toEqual(2)
+        expect(histories[0].result).toEqual(null)
+        expect(histories[0].Operation.name).toEqual("DIV")
     })
-})
+});
