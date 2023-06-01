6bfc478168bfbc7ccc4255218932e2258f8979ba
Sergio Adrian Maldonado <maldonado.systems@gmail.com>
2023-05-31 23:46:27 -0300

    Agrego el test para endpoint POW

--- test/api.test.js
+++ test/api.test.js
@@ -1,22 +1,105 @@
-const request = require('supertest');
-const api = require('../src/api.js');
-const { seed } = require('../src/seed.js')
+const request = require("supertest");
+const api = require("../src/api.js");
+const {seed} = require("../src/seed.js");
 
 beforeEach(async () => {
-    await seed()
-})
+  await seed();
+});
 
 describe("API substract", () => {
-    test("Deberia responder con un 200 ok", async () => {
-        const app = await api.build()
+  test("Deberia responder con un 200 ok", async () => {
+    const app = await api.build();
 
-        request(app).get('/api/v1/sub/2/1')
-            .expect(200)
-            .expect('Content-Type', "application/json; charset=utf-8")
-            .end((err, res) => {
-                if (err) throw err
+    const res = await request(app)
+      .get("/api/v1/sub/2/1")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
 
-                expect(res.body.result).toEqual(1);
-            })
-    })
+    expect(res.body.resultado).toEqual(1);
+  });
+});
+
+describe("API add", () => {
+  test("Deberia responder con un 200 ok", async () => {
+    const app = await api.build();
+
+    const res = await request(app)
+      .get("/api/v1/add/2/1")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toEqual(3);
+  });
+
+  test("Si el segundo parametro es negativo, resultado deberia ser menor al primer parametros", async () => {
+    const app = await api.build();
+    const a = 11;
+    const b = -8;
+
+    const res = await request(app)
+      .get(`/api/v1/add/${a}/${b}`)
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toBeLessThan(a);
+  });
+});
+
+describe("API MUL", () => {
+  test("Deberia responder con un 200 ok", async () => {
+    const app = await api.build();
+
+    const res = await request(app)
+      .get("/api/v1/multiply/4/2")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toEqual(8);
+  });
+
+  test("Si los parametros son decimales, resultado deberia ser decimal", async () => {
+    const app = await api.build();
+    const a = 2.5;
+    const b = 3.6;
+
+    const res = await request(app)
+      .get(`/api/v1/add/${a}/${b}`)
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    const resultado = res.body.resultado;
+    expect(Number.isInteger(resultado)).toBe(false);
+  });
+});
+
+describe("API DIV", () => {
+  test("Deberia responder con un 200 ok", async () => {
+    const app = await api.build();
+
+    const res = await request(app)
+      .get("/api/v1/div/4/2")
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+
+    expect(res.body.resultado).toEqual(2);
+  });
+
+  test("Si el segundo parametro es cero, debe dar error", async () => {
+    const app = await api.build();
+    const a = 4;
+    const b = 0;
+
+    if (b === 0) {
+      expect(() => {
+        throw new Error("Los parámetros no son válidos para la división");
+      }).toThrow();
+    } else {
+    
+    const res = await request(app)
+      .get(`/api/v1/div/${a}/${b}`)
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8");
+    const resultado = res.body.resultado;
+
+  }});
 })
--- test/models.test.js
+++ test/models.test.js
@@ -1,30 +1,117 @@
-const { seed } = require('../src/seed.js')
-const {
-    createHistoryEntry,
-    History,
-    Operation
-} = require('../src/models.js')
+const {seed} = require("../src/seed.js");
+const {createHistoryEntry, History, Operation} = require("../src/models.js");
 
 beforeEach(async () => {
-    await seed()
-})
+  await seed();
+});
 
 describe("History", () => {
-    test("Deberia poder crear una resta en el history", async () => {
-        await createHistoryEntry({
-            firstArg: 2,
-            secondArg: 2,
-            result: 0,
-            operationName: "SUB"
-        })
+  test("Deberia poder crear una resta en el history", async () => {
+    await createHistoryEntry({
+      firstArg: 2,
+      secondArg: 2,
+      result: 0,
+      operationName: "SUB",
+    });
 
-        const histories = await History.findAll({
-            include: [Operation]
-        })
+    const histories = await History.findAll({
+      include: [Operation],
+    });
 
-        expect(histories.length).toEqual(1)
-        expect(histories[0].firstArg).toEqual(2)
-        expect(histories[0].result).toEqual(0)
-        expect(histories[0].Operation.name).toEqual("SUB")
-    })
-})
+    expect(histories.length).toEqual(1);
+    expect(histories[0].firstArg).toEqual(2);
+    expect(histories[0].result).toEqual(0);
+    expect(histories[0].Operation.name).toEqual("SUB");
+  });
+
+  test("Debería persistir todos los argumentos ", async () => {
+    const entry = {
+      firstArg: 6,
+      secondArg: 2,
+      result: 4,
+      operationName: "SUB",
+    };
+    await createHistoryEntry(entry);
+
+    const histories = await History.findAll({
+      include: [Operation],
+    });
+
+    expect(histories.length).toEqual(1);
+    expect(histories[0].firstArg).toEqual(6);
+    expect(histories[0].secondArg).toEqual(2);
+    expect(histories[0].result).toEqual(4);
+    expect(histories[0].Operation.name).toEqual("SUB");
+  });
+
+  test("Debería devolver todo el historial ", async () => {
+    const entry = {
+      firstArg: 6,
+      secondArg: 2,
+      result: 4,
+      operationName: "SUB",
+    };
+    const entry2 = {
+      firstArg: 9,
+      secondArg: 1,
+      result: 8,
+      operationName: "SUB",
+    };
+    await createHistoryEntry(entry);
+    await createHistoryEntry(entry2);
+
+    const histories = await History.findAll();
+
+    expect(histories.length).toEqual(2);
+    expect(histories[0].firstArg).toEqual(6);
+    expect(histories[0].secondArg).toEqual(2);
+    expect(histories[0].result).toEqual(4);
+    expect(histories[1].firstArg).toEqual(9);
+    expect(histories[1].secondArg).toEqual(1);
+    expect(histories[1].result).toEqual(8);
+  });
+
+  test("Debería borrar todo el historial", async () => {
+    const entry = {
+      firstArg: 6,
+      secondArg: 2,
+      result: 4,
+      operationName: "SUB",
+    };
+    const entry2 = {
+      firstArg: 9,
+      secondArg: 1,
+      result: 8,
+      operationName: "SUB",
+    };
+    await createHistoryEntry(entry);
+    await createHistoryEntry(entry2);
+  
+    await History.destroy({ where: {} });
+  
+    const histories = await History.findAll();
+  
+    expect(histories.length).toEqual(0);
+  });
+  
+});
+
+test("Debería guardar el atributo 'error' en la base de datos", async () => {
+  const entry = {
+    firstArg: 6,
+    secondArg: 0,
+    result: null, // El resultado es nulo porque hay un error de división por cero
+    operationName: "DIV",
+    error: "Error: División por cero", // Nuevo atributo "error"
+  };
+  await createHistoryEntry(entry);
+
+  const history = await History.findOne({
+    where: {
+      firstArg: 6,
+      secondArg: 0,
+    },
+  });
+
+  expect(history.error).toEqual("Error: División por cero");
+});
