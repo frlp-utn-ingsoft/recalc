0b2c541085c264f1be9a9bab65f59a25a75a76a8
Sergio Adrian Maldonado <maldonado.systems@gmail.com>
2023-06-01 00:00:38 -0300

    Agregue persistencia en el historial de errores

--- src/controllers.js
+++ src/controllers.js
@@ -1,7 +1,7 @@
 import express from "express";
 import core from "./core.js";
 
-import { createHistoryEntry } from './models.js'
+import {createHistoryEntry, getHistorial, History} from "./models.js";
 
 const router = express.Router();
 
@@ -15,8 +15,13 @@ router.get("/sub/:a/:b", async function (req, res) {
   } else {
     const result = core.sub(a, b);
 
-    await createHistoryEntry({ firstArg: a, operationName: "ADD" })
-    return res.send({ result });
+    await createHistoryEntry({
+      firstArg: a,
+      secondArg: b,
+      result,
+      operationName: "SUB",
+    });
+    return res.send({resultado: result});
   }
 });
 
@@ -29,6 +34,12 @@ router.get("/add/:a/:b", async function (req, res) {
     res.status(400).send({error: "Uno de los parámetros no es un número"});
   } else {
     const result = core.add(a, b);
+    await createHistoryEntry({
+      firstArg: a,
+      secondArg: b,
+      result,
+      operationName: "ADD",
+    });
     return res.send({resultado: result});
   }
 });
@@ -44,6 +55,12 @@ router.get("/div/:a/:b", async function (req, res) {
       .send({error: "Los parámetros no son válidos para la división"});
   } else {
     const result = core.div(a, b);
+    await createHistoryEntry({
+      firstArg: a,
+      secondArg: b,
+      result,
+      operationName: "DIV",
+    });
     return res.send({resultado: result});
   }
 });
@@ -59,6 +76,12 @@ router.get("/multiply/:a/:b", async function (req, res) {
       .send({error: "Los parámetros no son válidos para el producto"});
   } else {
     const result = core.mul(a, b);
+    await createHistoryEntry({
+      firstArg: a,
+      secondArg: b,
+      result,
+      operationName: "MUL",
+    });
     return res.send({resultado: result});
   }
 });
@@ -75,6 +98,21 @@ router.get("/pow/:a/:b", async function (req, res) {
   } else {
     const result = core.pow(a, b);
     return res.send({resultado: result});
+  }
+});
+
+router.get("/all", async function (req, res) {
+  const historial = await getHistorial();
+  return res.send({historial});
+});
+
+router.get("/delete-all", async function (req, res) {
+  try {
+    await deleteHistorial(); // Llamada a la función para eliminar todo el historial
+    return res.send("Historial eliminado correctamente");
+  } catch (error) {
+    console.error("Error al eliminar el historial:", error);
+    return res.status(500).send("Error al eliminar el historial");
   }
 });
 
--- src/models.js
+++ src/models.js
@@ -1,55 +1,105 @@
-import { Sequelize, DataTypes } from 'sequelize';
+import {Sequelize, DataTypes} from "sequelize";
 
-const inTest = process.env.NODE_ENV === 'test';
+const inTest = process.env.NODE_ENV === "test";
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    logging: !inTest,
-    storage: inTest ? ':memory:' : './db.sqlite3'
-})
-
-export const History = sequelize.define('History', {
-    firstArg: {
-        type: DataTypes.NUMBER,
-        allowNull: false
-    },
-    secondArg: {
-        type: DataTypes.NUMBER,
-        allowNull: true
-    },
-    result: {
-        type: DataTypes.NUMBER,
-        allowNull: true
-    }
+  dialect: "sqlite",
+  logging: !inTest,
+  storage: inTest ? ":memory:" : "./db.sqlite3",
 });
 
-export const Operation = sequelize.define('Operation', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
+export const History = sequelize.define("History", {
+  firstArg: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  secondArg: {
+    type: DataTypes.NUMBER,
+    allowNull: true,
+  },
+  result: {
+    type: DataTypes.NUMBER,
+    allowNull: true,
+  },
 });
 
-Operation.hasMany(History)
-History.belongsTo(Operation)
+export const Operation = sequelize.define("Operation", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
-export async function createHistoryEntry({ firstArg, secondArg, operationName, result }) {
-    const operation = await Operation.findOne({
-        where: {
-            name: operationName
-        }
-    });
+Operation.hasMany(History);
+History.belongsTo(Operation);
 
-    return History.create({
-        firstArg,
-        result,
-        OperationId: operation.id
-    })
+export async function createHistoryEntry({
+  firstArg,
+  secondArg,
+  operationName,
+  result,
+}) {
+  const operation = await Operation.findOne({
+    where: {
+      name: operationName,
+    },
+  });
+
+  return History.create({
+    firstArg,
+    secondArg,
+    result,
+    OperationId: operation.id,
+  });
+}
+
+export async function getHistorial() {
+  return History.findAll();
+}
+
+export async function deleteHistorial() {
+  await History.destroy({
+    where: {},
+    truncate: true
+  });
 }
 
 export function createTables() {
-    return Promise.all([
-        History.sync({ force: true }),
-        Operation.sync({ force: true })
-    ]);
+  return Promise.all([
+    History.sync({force: true}),
+    Operation.sync({force: true}),
+  ]);
 }
+
+class History extends Model {}
+History.init(
+  {
+    firstArg: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    secondArg: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    result: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    operationName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    error: {
+      type: DataTypes.TEXT, // Agrega el nuevo atributo "error" del tipo texto
+    },
+  },
+  {
+    sequelize,
+    modelName: "History",
+  }
+);
+
+// Resto del código del modelo...
+
+module.exports = { History, Operation };
