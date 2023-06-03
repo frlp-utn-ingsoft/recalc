1ad1f44e1f6378c5e0a549a57a1253b16498516f
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2023-06-03 07:47:29 -0300

    Agrega controller para 'add'

--- src/controllers.js
+++ src/controllers.js
@@ -20,4 +20,19 @@ router.get("/sub/:a/:b", async function (req, res) {
     }
 });
 
+router.get("/add/:a/:b", async function (req, res) {
+    const params = req.params;
+    const a = Number(params.a);
+    const b = Number(params.b);
+
+    if (isNaN(a) || isNaN(b)) {
+        res.status(400).send('Uno de los parámetros no es un número');
+    } else {
+        const result = core.add(a, b);
+
+        await createHistoryEntry({ firstArg: a, secondArg: b, result, operationName: "ADD" })
+        return res.send({ result });
+    }
+});
+
 export default router;
